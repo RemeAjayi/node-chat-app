49df3f0e767009f782b697110d4520e8cc7f1a0e
Olohireme Ajayi <remeajayi@gmail.com>
2018-10-01 22:28:00 +0100

    added io.emit for create messages2

--- public/js/index.js
+++ public/js/index.js
@@ -2,10 +2,7 @@ var socket = io();
 socket.on('connect', function (){
 console.log('Connected to Server');
 
-socket.emit('createMessage',  {
-to:'jen@example.com',
-text: 'hey, this is Reme'
-});
+
 
 });
 socket.on('disconnect', function(){
--- server/server.js
+++ server/server.js
@@ -15,14 +15,15 @@ app.use(express.static(publicPath));
 io.on('connection', (socket)=>{
 console.log('New user connected');
 
-socket.emit("newMessage", {
-  from: 'mike@example.com',
-  text:'hey, what\'s up',
-  timestamp: 123
-});
 //must match the listener in the client javascript
-socket.on('createMessage', function(newMessage){
-console.log('Create Message', newMessage);
+socket.on('createMessage', function(message){
+console.log('Create Message', message);
+io.emit('newMessage', {
+  from: message.from,
+  text: message.text,
+  createdAt: new Date().getTime()
+});
+
 });
 
 socket.on('disconnect', ()=>{
